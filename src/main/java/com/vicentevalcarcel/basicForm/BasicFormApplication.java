5895ecd61cebc54e56b759f2d78c4f6030dc1dd6
vicentevalcarcel <vicente@>
2024-12-09 00:15:55 -0300

    first commit

---
+++ src/main/java/com/vicentevalcarcel/basicForm/BasicFormApplication.java
@@ -0,0 +1,13 @@
+package com.vicentevalcarcel.basicForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicentevalcarcel/basicForm/BasicFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicentevalcarcel.basicForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
